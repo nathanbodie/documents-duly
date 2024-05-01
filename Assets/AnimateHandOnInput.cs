b757cc913a5896f1adc5675cfb3708bc68f462ea
Trent Halama <83091463+Gipper1998@users.noreply.github.com>
2024-04-30 21:59:51 -0400

    Works
    Working version 2021

---
+++ Assets/AnimateHandOnInput.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AnimateHandOnInput : MonoBehaviour
+{
+    // Get hand input properties for animator.
+    public InputActionProperty pinchAnimationAction;
+    public InputActionProperty gripAnimationAction;
+    public Animator handAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Set the trigger and grip values.
+        float triggerValue = pinchAnimationAction.action.ReadValue<float>();
+        handAnimator.SetFloat("Trigger", triggerValue);
+
+        float gripValue = gripAnimationAction.action.ReadValue<float>();
+        handAnimator.SetFloat("Grip", gripValue);
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,23 +1,126 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.InteropServices;
 using UnityEngine;
-
+using Random = UnityEngine.Random;
 
 public class GameManager : MonoBehaviour
 {
+    //GameObject Passport = GameObject.Find("PassportScriptable");
+    public GameObject gameOver;
+
+    // 0 - 2 = knaveria, 3 - 5 = milia, 6 - 8 = witha
     [SerializeField] Sprite[] passportImages;
     [SerializeField] SpriteRenderer spriteRenderer;
-    int randomNumber;
-    void Start()
+
+    public Character current = null;
+    Character char1 = new Character { allowed = true, country = "Knaveria", bodyType = "pyramidbuff" };
+    Character char2 = new Character { allowed = false, country = "Knaveria", bodyType = "pyramidbuff" };
+    Character char3 = new Character { allowed = false, country = "Milia", bodyType = "cubechunk" };
+    Character char4 = new Character { allowed = false, country = "Withambian", bodyType = "spheretiny" };
+    Character char5 = new Character { allowed = true, country = "Milia", bodyType = "cubechunk" };
+    Character char6 = new Character { allowed = true, country = "Withambian", bodyType = "spheretiny" };
+    List<Character> characters = new List<Character>();
+
+
+    public void loadCharacter()
     {
-        //ThrowTheDice();
+        current = characters.ElementAt(UnityEngine.Random.Range(0, characters.Count()));
+        PrintPassport();
+    }
+        
+    public void OnStartButtonPress()
+    {
+        characters.Add(char1);
+        characters.Add(char2);
+        characters.Add(char3);
+        characters.Add(char4);
+        characters.Add(char5);
+        characters.Add(char6);
+        loadCharacter();
+    }
+
+    public void OnGreenButtonPress()
+    {
+        if (current.allowed == true)
+        {
+            loadCharacter();
+        }
+        else
+        {
+            // Display gameover.
+        }
+    }
+
+    public void OnRedButtonPress()
+    {
+        if (current.allowed == false)
+        {
+            loadCharacter();
+        }
+        else
+        {
+            // Display gameover.
+        }
     }
 
 
-    public void ThrowTheDice()
+    public void PrintPassport()
     {
-        randomNumber = Random.Range(1, passportImages.Length + 1);
-        spriteRenderer.sprite = passportImages[randomNumber - 1];
-        print(randomNumber);
+        int start = 0;
+        if (current.country.Equals("Knaveria") == true)
+        {
+            start = 0;
+            if (current.allowed == true)
+            {
+                spriteRenderer.sprite = passportImages[start];
+            }
+            else
+            {
+                int ran = UnityEngine.Random.Range(1, 3);
+                spriteRenderer.sprite = passportImages[start + ran];
+            }
+        }
+
+        else if (current.country.Equals("Milia") == true)
+        {
+            start = 3;
+            if (current.allowed == true)
+            {
+                spriteRenderer.sprite = passportImages[start];
+            }
+            else
+            {
+                int ran = UnityEngine.Random.Range(1, 3);
+                Debug.Log(ran);
+                spriteRenderer.sprite = passportImages[start + ran];
+            }
+        }
+
+        else if (current.country.Equals("Withambian") == true)
+        {
+            start = 6;
+            Debug.Log("Goes here");
+            if (current.allowed == true)
+            {
+                spriteRenderer.sprite = passportImages[start];
+            }
+            else
+            {
+                int ran = UnityEngine.Random.Range(1, 3);
+                spriteRenderer.sprite = passportImages[start + ran];
+            }
+        }
     }
+}
+
+public class Character
+{
+    public bool allowed;
+    public string country;
+    public string bodyType;
 }
